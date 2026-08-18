62c74ac123285d8bda6c262d3608dc7e99c64f23
Ritika Gupta <ritikagupta8091@gmail.com>
2026-08-18 23:29:18 +0530

    change done

--- bdd-framework-A1/src/test/java/base/BaseTest.java
+++ bdd-framework-A1/src/test/java/base/BaseTest.java
@@ -3,18 +3,24 @@ package base;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
-	public static WebDriver driver;
-	
-	public void setUp() {
-		driver = new EdgeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-	}
-	
-	public void tearDown() {
-		driver.quit();
-	}
+
+    public static WebDriver driver;
+
+    public void setUp() {
+
+        driver = new ChromeDriver();
+
+       
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
